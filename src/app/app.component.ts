5eea2a0afc8312acce9d7e4b4a2d5e1f08ddb685
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-10 20:37:31 +0000

    Added Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from './components/item-list/item-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-tasksify';
+export class AppComponent implements OnInit {
+  tasks: Task[] = [];
+
+  ngOnInit() {}
+
+  addItem(title: string) {
+    this.tasks.push({
+      title
+    });
+  }
 }
